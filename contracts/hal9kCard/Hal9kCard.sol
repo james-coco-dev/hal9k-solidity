d29c14f63690693caecf9d272d0b4919bd7d5e2e
super-talent-aj <my.hercules.gj@gmail.com>
2020-11-09 17:14:10 -0800

    Update hal9k card name

--- contracts/hal9kCard/Hal9kCard.sol
+++ contracts/hal9kCard/Hal9kCard.sol
@@ -4,9 +4,9 @@ import './ERC1155Tradable.sol';
 
 /**
  * @title MemeLtd
- * MemeLtd - Collect limited edition NFTs from HAL9KCard
+ * MemeLtd - Collect limited edition NFTs from HAL9KLtd
  */
-contract HAL9KCard is ERC1155Tradable {
+contract HAL9KLtd is ERC1155Tradable {
 	constructor(address _proxyRegistryAddress) public ERC1155Tradable("H9KNFT", "H9K", _proxyRegistryAddress) {
 		_setBaseMetadataURI("https://api.hal9k.ai/hals");
 	}
