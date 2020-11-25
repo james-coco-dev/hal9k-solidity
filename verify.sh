93aad6b82ecd3bad1f560595e1e595157b3f26a1
super-talent-aj <my.hercules.gj@gmail.com>
2020-11-25 18:46:32 -0500

    update tokens

--- verify.sh
+++ verify.sh
@@ -1,8 +1,8 @@
 echo "Verifying contracts"
 echo "⌛ Verifying Hal9k Token"
-npx hardhat verify --network rinkeby 0x80aCE96aB5a40F110c9477460c77004CA16669a2 '0x7a250d5630b4cf539739df2c5dacb4c659f2488d' '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+npx hardhat verify --network rinkeby 0xf16447C1FD9BADEfa795Ab4EBF9b72DCBd91EF2B '0x7a250d5630b4cf539739df2c5dacb4c659f2488d' '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 echo "⌛ Verifying Hal9k Ltd"
-npx hardhat verify --network rinkeby 0x6aFb66f0D3188e400A4bBFA589CfF01E6c9F91b3 '0xf57b2c51ded3a29e6891aba85459d600256cf317'
+npx hardhat verify --network rinkeby 0xeBB4e3EF1F7DaF0F241fc6e005562470cB8c7620 '0xf57b2c51ded3a29e6891aba85459d600256cf317'
 echo "⌛ Verifying Hal9k Vault"
 npx hardhat verify --network rinkeby 0x18642D8Ba0FAAfE02913B8DA6b2D793EbF7e017b
 echo "⌛ Verifying Fee Approver"
